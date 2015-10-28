f3ef8021f6d3eb88beb6424fb8472633eac2835b
Akash Verma <akash1630@gmail.com>
2015-10-28 18:13:44 -0400

    login modal related changes

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,18 +4,51 @@ var AFControllers = angular.module("AFControllers", [
 		'ui.bootstrap'
 	]);
 
+AFControllers.factory('sharedService', function() {
 
-AFControllers.controller("AFHeaderCtrl", [ '$scope', '$modal', function($scope, $modal) {
+	console.log('inside shared service');
+    this.loginModalActive = false;
+    this.tabActiveOnModal = "login";
+
+    return {
+    	setLoginModalActive : function(active){
+	    	console.log('setting modal active');
+	    	this.loginModalActive = active;
+	    },
+	    
+	    getLoginModalActive : function(){
+	    	console.log('getting modalactive');
+	    	return this.loginModalActive;
+	    },
+
+	    setTabActiveOnModal : function(tab){
+	    	console.log('setting tab active');
+	    	this.tabActiveOnModal = tab;
+	    },
+
+	    getTabActiveOnModal : function(){
+	    	console.log('getting tab active');
+	    	return this.tabActiveOnModal;
+	    }
+    }
+    
+});
+
+AFControllers.controller("AFHeaderCtrl", [ '$scope', '$modal', 'sharedService', function($scope, $modal, sharedService) {
 	$scope.animationsEnabled = true;
-
 	$scope.open = function (modaltype) {
-
+		console.log('opening modal for ' + modaltype);
+		sharedService.setLoginModalActive(true);
+		sharedService.setTabActiveOnModal(modaltype);
+		console.log(angular.element(document.getElementById(modaltype)));
 		var modalInstance = $modal.open({
 		  animation: $scope.animationsEnabled,
 		  templateUrl: 'login.html',
-		  controller: 'ModalInstanceCtrl',
-		  windowClass: 'login-modal-class'
+		  controller: 'loginModalInstanceCtrl',
+		  windowClass: 'login-modal-class',
+		  currentActive : modaltype
 		});
+		
 	}
 
 	$scope.toggleAnimation = function () {
@@ -23,15 +56,18 @@ AFControllers.controller("AFHeaderCtrl", [ '$scope', '$modal', function($scope, 
 	};
 }]);
 
-AFControllers.controller('loginModalInstanceCtrl', ['$scope', '$modalInstance', function($scope, $modalInstance) {
+AFControllers.controller('loginModalInstanceCtrl', ['$scope', '$modalInstance', 'sharedService', function($scope, $modalInstance, sharedService) {
 	
-	$scope.ok = function () {
-		$modalInstance.dismiss('ok');
-	};
-
-	$scope.cancel = function () {
-	$modalInstance.dismiss('cancel');
-	};
+	$scope.setActive = function(tab){
+		$scope.currentTabActive = sharedService.getTabActiveOnModal();
+		console.log('setting active tab : ' + tab);
+		console.log(angular.element(document.getElementById(tab)));
+		angular.element(document.getElementById($scope.currentTabActive)).removeClass('visible');
+		angular.element(document.getElementById(tab)).addClass('visible');
+		angular.element(document.getElementById(tab + '-tab')).addClass('tab-active');
+		angular.element(document.getElementById($scope.currentTabActive  + '-tab')).removeClass('tab-active');
+		sharedService.setTabActiveOnModal(tab);
+	}
 }]);
 
 AFControllers.controller("mainContentCtrl", function($scope) {
